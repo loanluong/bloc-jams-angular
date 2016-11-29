1e9b6c0fe3d2e016224419cafd1be1fbeaf089ee
Lani Luong <loanlu@gmail.com>
2016-11-29 14:30:32 -0800

    assignment-3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,22 +1,29 @@
-(function() {
-     function config($stateProvider, $locationProvider) {
-         $locationProvider
-            .html5Mode({
-                enabled: true, 
-                requireBase: false
+(function() { 
+function config($stateProvider, $locationProvider) {
+     $locationProvider
+         .html5Mode({
+             enabled: true,
+             requireBase: false
          });
-         
-         $stateProvider
-            .state('landing', {
-                url: '/',
-                templateUrl: '/templates/landing.html'
+
+     $stateProvider
+         .state('landing', {
+             url: '/',
+             templateUrl: '/templates/landing.html'
          })
+ 
          $stateProvider
             .state('album', {
                 url: '/album',
                 templateUrl: '/templates/album.html'
+         })
+         $stateProvider
+            .state('collection', {
+             url: '/collection',
+             tempateUrl: '/templates/collection.html'
          });
      }
+});
 
 angular 
     .module('blocJams', ['ui.router']);
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,32 @@
-angular.module('blocJams', []);
+(function() { 
+function config($stateProvider, $locationProvider) {
+     $locationProvider
+         .html5Mode({
+             enabled: true,
+             requireBase: false
+         });
+
+     $stateProvider
+         .state('landing', {
+             url: '/',
+             templateUrl: '/templates/landing.html'
+         })
+ 
+         $stateProvider
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+         })
+         $stateProvider
+            .state('collection', {
+             url: '/collection',
+             tempateUrl: '/templates/collection.html'
+         });
+     }
+});
+
+angular 
+    .module('blocJams', ['ui.router']);
+    .config(config);
+    
+})();
